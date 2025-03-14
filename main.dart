35733cdc39b76df6c1d5759258f259aaf7ef1ff3
JANGCHUALHWA <rkduddlrnss@naver.com>
2025-03-15 00:38:47 +0900

    Hello World Flutter

--- main.dart
+++ main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello World Flutter',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hello World Flutter'),
     );
   }
 }
